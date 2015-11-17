c563a8732b517547bfafe49935d79a18b5aa3e7d
YanagiEiichi <admin@web-tinker.com>
2015-11-18 03:07:41 +0800

    Add "finite provider" command

--- index.js
+++ index.js
@@ -4,13 +4,26 @@ var argollector = require('argollector');
 var bfs = require('babel-fs');
 var path = require('path');
 
-// 查看版本号
-if(argollector['-v'] || argollector['--version']) {
-  bfs.readFile(path.join(__dirname, 'package.json')).then(function(data) {
-    console.log(JSON.parse(data).version);
-    process.exit(0);
-  }).catch(function(error) {
-    console.error(error);
-    process.exit(1);
-  });
-}
+void function() {
+
+  // 查看版本号
+  if(argollector['-v'] || argollector['--version']) {
+    return bfs.readFile(path.join(__dirname, 'package.json')).then(function(data) {
+      console.log(JSON.parse(data).version);
+    });
+  }
+
+  // 命令处理
+  switch(argollector[0]) {
+    case 'provider':
+      return bfs.readdir(path.join(__dirname, 'Makefile.d')).then(function(list) {
+        list.forEach(function(item) { console.log(item); });
+      });
+  }
+
+}().then(function() {
+  process.exit(0);
+}).catch(function(error) {
+  console.error(error);
+  process.exit(1);
+});
