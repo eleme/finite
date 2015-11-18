54e0121e418e87825db77639ec27130d368c8ddc
YanagiEiichi <admin@web-tinker.com>
2015-11-18 15:33:15 +0800

    Fix finite-dir

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ void function() {
       var lib = path.join(__dirname, 'Makefile.d');
       return bfs.readdir(lib).then(function(list) {
         list.forEach(function(item) {
-          if(/[^\w-]/.test(item)) return;
+          if(/^\./.test(item)) return;
           console.log(path.join(lib, item));
         });
       });
